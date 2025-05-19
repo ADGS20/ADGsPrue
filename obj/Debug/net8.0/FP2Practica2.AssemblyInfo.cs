de28d6d1e8c6804267c631e283b829167ec57498
ADGS20 <diazguerreroandres@gmail.com>
2025-05-19 18:46:18 +0200

    vamos a corregir correctamente
    ja, ja, ja...

--- obj/Debug/net8.0/FP2Practica2.AssemblyInfo.cs
+++ obj/Debug/net8.0/FP2Practica2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FP2Practica2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d098bf784a4e117ebacaa1d09ac462700341b83")]
 [assembly: System.Reflection.AssemblyProductAttribute("FP2Practica2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FP2Practica2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
